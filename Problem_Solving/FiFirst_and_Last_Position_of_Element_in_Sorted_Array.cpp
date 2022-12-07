ba67c2afd327819d9a952e7c1b81ba1632d863fc
ashishverma4822 <35832574+ashishverma4822@users.noreply.github.com>
2022-12-07 14:45:01 +0530

    Create FiFirst_and_Last_Position_of_Element_in_Sorted_Array.cpp

---
+++ Problem_Solving/FiFirst_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -0,0 +1,42 @@
+class Solution {
+//Ashish Kumar Verma
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int s=0;
+        int e=nums.size()-1;
+        int mid=s+(e-s)/2;
+        int ans1=-1,ans2=-1;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                e=mid-1;
+                ans1=mid;
+            }
+            else if(nums[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        s=0;
+        e=nums.size()-1;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            if(nums[mid]==target){
+                s=mid+1;
+                ans2=mid;
+            }
+            else if(nums[mid]<target){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        vector<int>ans;
+        ans.push_back(ans1);
+        ans.push_back(ans2);
+        return ans;
+    }
+};

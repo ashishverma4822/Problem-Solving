c2666a2f997fc7115d481eb24841acbab6d336a5
ashishverma4822 <35832574+ashishverma4822@users.noreply.github.com>
2023-04-14 00:14:14 +0530

    Add files via upload

---
+++ Stack_Question/NGL.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> NGL(vector<int>v){
+    stack<int>st;
+    vector<int> res;
+    for(int i=0;i<v.size();i++){
+        if(st.empty()){
+            res.push_back(-1);
+        }
+        else if(!st.empty() and st.top()>v[i]){
+            res.push_back(st.top());
+        }
+        else if(!st.empty() and st.top()<=v[i]){
+            while(!st.empty() and st.top()<=v[i]){
+                st.pop();
+            }
+            if(st.empty()) res.push_back(-1);
+            else res.push_back(st.top());
+        }
+        st.push(v[i]);
+    }
+    return res;
+}
+int main(){
+    int n;
+    cout<<"Enter The Size Of Array"<<endl;
+    cin>>n;
+    vector<int>v;
+    cout<<"Enter The Array Element"<<endl;
+    for(int i=0;i<n;i++){
+        int t;
+        cin>>t;
+        v.push_back(t);
+    }
+    cout<<"The Array Elements Are :: ";
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    vector<int>res=NGL(v);
+    cout<<endl;
+    cout<<"The NGL is :: ";
+    for(int i=0;i<n;i++){
+        cout<<res[i]<<" ";
+    }
+    return 0;
+}
